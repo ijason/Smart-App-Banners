cbccfb1ae8bbc9784dbb43829feff77f4603ac7f
kdrdgn <kadir.doggan@gmail.com>
2013-12-30 23:10:34 +0200

    used regexp for extract appID. meta[name=apple-itunes-app] content attribute can contain additiona parameters like app-argument.
    CloseSmartBanner function removed and binded click event to the swb-close element.

--- js/smart-app-banner.js
+++ js/smart-app-banner.js
@@ -9,7 +9,7 @@ $(function() {
   var appBannerID = $('meta[name=apple-itunes-app]').attr("content"); //Check if using smart app banners
   if (!standalone && safari) { safari = false}; //Chrome is just a re-skinning of iOS WebKit UIWebView
   if (appBannerID != null) { 
-    appBannerID = appBannerID.replace('app-id=',''); 
+    appBannerID = /app-id=([0-9]+)/.exec(appBannerID)[1]; // maybe developers add some parameter like that app-argument=http://domain.com/path/to/page
     if ((iPad || iPhone) && (!safari)) {
       $.getJSON('http://itunes.apple.com/lookup?id=' + appBannerID + '&callback=?', function(json) {
         if (json != null) {
@@ -27,7 +27,7 @@ $(function() {
           if (averageUserRatingForCurrentVersion == null) { averageUserRatingForCurrentVersion = 0; }
 
           var banner = '<div class="smart-banner">';  
-          banner += '<a href="#" id="swb-close" onclick="CloseSmartBanner()">X</a>';
+          banner += '<a href="#" id="swb-close">X</a>';
           banner += '<img src="' + artworkUrl60 + '" alt="" class="smart-glossy-icon" />';
           banner += '<div id="swb-info"><strong>' + trackCensoredName + '</strong>';
           banner += '<span>' + artistName + '</span>';
@@ -35,16 +35,18 @@ $(function() {
           banner += '<span>' + formattedPrice + '</span></div>';
           banner += '<a href="' + artistViewUrl + '" id="swb-save">VIEW</a></div>';
 
-          $('body').append(banner);                       
+          $('body').append(banner);    
+          
+          $('#swb-close').click(function(e){
+            e.preventDefault();
+            $('.smart-banner').stop().animate({top:'-82px'},300);
+            $('html').animate({marginTop:origHtmlMargin},300);
+          });
+            
           $('.smart-banner').stop().animate({top:0},300);
           $('html').animate({marginTop:origHtmlMargin+78},300); 
         }
       }); 
     }
   }       
-}); 
-      
-function CloseSmartBanner() {
-  $('.smart-banner').stop().animate({top:-82},300);
-  $('html').animate({marginTop:origHtmlMargin},300);
-} 
+});
